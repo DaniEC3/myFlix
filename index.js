2bf1080ed5a1e7411ac020030bdefb4d9b857c6c
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2024-11-09 21:50:04 -0500

    CORS Corrected
    All domains corrected

--- index.js
+++ index.js
@@ -33,22 +33,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 app.use(morgan("combined", { stream: accessLogStream }));
 
 const cors = require('cors');
-
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-// Creates a list of allowed domains within the variable allowedOrigins
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+app.use(cors());
 
 // Authentication setup
 let auth = require("./auth")(app); // This ensures that Express is available in your "auth.js" file as well.
